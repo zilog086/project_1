c9340e5e9da8d4b666848154f27069cdf1fcfb36
Zilog <email@gmail.com>
2020-11-09 11:39:16 +0500

    Function

--- js/script.js
+++ js/script.js
@@ -1,40 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt('Skolko filmov posmotreli ? :', 0);
+const sum = (num1,num2) => (num1 + num2);
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+console.log(sum(5, 6));
 
-if (personalMovieDB.count < 10) {
-  alert('Prosmotreno malo filmov');
-} else if (personalMovieDB.count >=10 && personalMovieDB.count <= 30) {
-  alert('Vi classic zritel');
-} else if (personalMovieDB.count > 30) {
-  alert('Vi kinoman');
-} else {
-  alert('Proizoshla oshibka');
-}
-
-let i = 0;
-do {
-  let filmName = '',
-      filmMark = '';
-
-  while (filmName === '' || filmName === null || filmName.length > 50) {
-    filmName = prompt('Odin iz poslednih vashih filmov ? :', '');
-  }
-  while (filmMark === '' || filmMark === null) {
-    filmMark = prompt('Vo skolko otsenite film ? :', '');
-  }
-     
-  personalMovieDB.movies[filmName] = filmMark;
-
-  i++;
-} while (i < 2);
-
-console.log(personalMovieDB);
